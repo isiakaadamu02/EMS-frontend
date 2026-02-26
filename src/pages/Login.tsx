e40d9b3200b5f498d64bbe3e74722761f5e03d99
isiakaadamu02 <isiakaadamu161@gmail.com>
2026-02-26 16:02:16 -0600

    login correction

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
     const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://localhost:3000/api/auth/login", {email, password});
+            const response = await axios.post("https://ems-server-cyan.vercel.app/api/auth/login", {email, password});
             if(response.data.success) {
                 console.log("Successfully Logged In")
                 login(response.data.user)
